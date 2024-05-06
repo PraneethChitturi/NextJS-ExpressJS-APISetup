2031cad624a674886f95ac20ec9b736e3015468b
PraneethChitturi <praneethchitturi12@gmail.com>
2024-05-05 17:06:25 -0700

    SetupAPIs

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,25 @@
-console.log('Express Tutorial')
+const express = require('express');
+const path = require('path')
+
+const app = express()
+
+const logger = ()=>{
+    const method = req.method
+    const url = req.url
+    const time = new Date().getFullYear()
+
+    console.log(method,url,time)
+}
+
+app.get("/",logger,(req,res)=>{
+
+    res.send('Home')
+}) 
+
+app.get("/about",(req,res)=>{
+    res.send('About')
+})
+
+app.listen(5000,()=>{
+    console.log('server is listening on port 5000...')
+})
